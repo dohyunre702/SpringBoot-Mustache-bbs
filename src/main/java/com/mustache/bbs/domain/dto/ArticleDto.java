7e17fe981a4abd14b984079c2078c412fc50d38d
dohyu <dohyunre702@gmail.com>
2022-11-07 14:19:12 +0900

    ArticleDto 생성

---
+++ src/main/java/com/mustache/bbs/domain/dto/ArticleDto.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs.domain.dto;
+
+public class ArticleDto {
+    private Long id;
+    private String title;
+    private String content;
+
+    public ArticleDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+}
