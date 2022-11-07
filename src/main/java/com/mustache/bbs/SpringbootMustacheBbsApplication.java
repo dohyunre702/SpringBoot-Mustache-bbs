bd283b16fe3d9a3e8d9dc660b761c9921090f5e2
dohyu <dohyunre702@gmail.com>
2022-11-07 11:16:23 +0900

    first commit

---
+++ src/main/java/com/mustache/bbs/SpringbootMustacheBbsApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustacheBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMustacheBbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustache/bbs/SpringbootMustacheBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustacheBbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
