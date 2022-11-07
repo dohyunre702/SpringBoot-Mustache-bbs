e82c0afc2136cb820cc2c0f5f297621735c3e911
dohyu <dohyunre702@gmail.com>
2022-11-07 12:17:32 +0900

    1 유저명 수정

--- src/main/java/com/mustache/bbs/controller/MustacheController.java
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -2,13 +2,13 @@ package com.mustache.bbs.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller //컨트롤러 선언
 public class MustacheController {
-
     @GetMapping(value="/hi") //endpoint 매핑
     public String mustacheCon(Model model) { //Model model 파라미터
-        model.addAttribute("username", "rok"); //view에 값을 넘기겠다.
+        model.addAttribute("username", "dh"); //view에 값을 넘기겠다.
         return "greetings"; //view를 리턴
     }
 }
