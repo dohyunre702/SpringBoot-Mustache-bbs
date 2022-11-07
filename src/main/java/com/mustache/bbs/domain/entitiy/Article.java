22ae908d5cec42986d88c2e8888776c9a40ee7c1
dohyu <dohyunre702@gmail.com>
2022-11-07 14:50:38 +0900

    article entity 생성 -id 추가

--- src/main/java/com/mustache/bbs/domain/entitiy/Article.java
+++ src/main/java/com/mustache/bbs/domain/entitiy/Article.java
@@ -1,8 +1,12 @@
 package com.mustache.bbs.domain.entitiy;
 
+import org.springframework.data.annotation.Id;
 import javax.persistence.Entity;
 
 @Entity
 public class Article {
+
+    @Id
+    private Long id;
 
 }
