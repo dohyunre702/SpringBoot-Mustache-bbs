9908c3771a8708a74073f6e8b8da0893f22c7e44
dohyu <dohyunre702@gmail.com>
2022-11-08 15:29:55 +0900

    코드 수정 및 템플릿 매핑 - add 수정 - findall 추가 - list 추가 - template 연결

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -5,13 +5,17 @@ import com.mustache.bbs.domain.entitiy.Article;
 import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
 @Slf4j
 public class ArticleController {
-
+    //Spring이 ArticleRepository 구현체를 DI(인터페이스 아님)
     private final ArticleRepository articleRepository;
 
     //autowired보다 생성자로 주입하는 형식을 더 많이 씀
@@ -19,12 +23,40 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
+    // /list일 때
+    @GetMapping("/list")
+    public String list(Model model) { //파라미터로 모델 연결. list에 값 넘겨주기
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "list";
+    }
+
+    // /일 때
+    @GetMapping("")
+    public String index() {
+        return "redirect:/articles/list";
+    }
+
     @GetMapping(value="/new")
     public String newArticleForm() {///articles/new url로 들어오면 artices/new.mustache를 출력
         return "articles/new";
     }
 
-    @PostMapping(value="/new") //articles
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        //잘 들어갔을 때 show로 가도록 라우팅
+        if(!optArticle.isEmpty()) {
+            //Optional.get() --> Article
+            model.addAttribute("article", optArticle.get());
+            return "show";
+        } else {
+            return "error";
+        }
+    }
+
+    @PostMapping(value="") //articles
     public String createArticle(ArticleDto form) {
         //로그 : 서버에서 일어나는 일 기록 (not println)
         log.info(form.toString());
@@ -33,4 +65,12 @@ public class ArticleController {
         return "";
     }
 
+    @PostMapping("") //다른 페이지로 이동처리
+    public String add(ArticleDto articleDto) {
+        log.info(articleDto.getTitle());
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        log.info("generatedId:{}", savedArticle.getId());
+
+        return String.format("redirect:/articles/%d", savedArticle.getId());
+    }
 }
--- src/main/java/com/mustache/bbs/repository/ArticleRepository.java
+++ src/main/java/com/mustache/bbs/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.mustache.bbs.repository;
 
 import com.mustache.bbs.domain.entitiy.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 //리포지토리 사용
+@Repository
 public interface ArticleRepository  extends JpaRepository<Article,Long> {
 }
